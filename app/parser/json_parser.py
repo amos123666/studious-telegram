a563e734a2aa4b1359a6af6a655ee159ca587c87
fraser <frasergilbert184@gmail.com>
2021-08-14 14:16:38 +0800

    Create json_parser.py
    loads in a data file in json format

---
+++ app/parser/json_parser.py
@@ -0,0 +1,30 @@
+import json
+from .parser import AbstractParser
+
+
+class JsonLoader(AbstractParser):
+
+    def read_data(file):
+
+        # load in json data
+        with open(file, 'r') as fp:
+            data = json.load(fp)
+
+        # create dict and store json data
+        dict_q = {}
+        dict_q['questions'] = []
+        for q in data['questions']:
+            dict_q['questions'].append({
+                'date': q['date'],
+                'to': q['to'],
+                'Received': q['Received'],
+                'subject':  q['subject'],
+                'from': q['from'],
+                'x-smilie': q['x-smilie'],
+                'x-img': q['x-img'],
+                'text_body': q['text_body'],
+                'subject_embedding': q['subject_embedding'],
+                'text_embedding': q['text_embedding'],
+                'answers': q['answers']
+            })
+        return dict_q

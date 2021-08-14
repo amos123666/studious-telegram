565caa0d490b5ae703cf4072faf6e9d44a17e6e0
Shana Edwards <82807298+shana-edwards@users.noreply.github.com>
2021-08-14 12:51:01 +0800

    adding documentation to keywordmatcher (#16)

--- app/domain/keywordmatcher.py
+++ app/domain/keywordmatcher.py
@@ -2,12 +2,29 @@ from .questionmatcher import AbstractQuestionMatcher
 from typing import List
 
 class KeywordMatcher(AbstractQuestionMatcher):
+    '''
+    Class matching suggestions to a question, based off specific key words.
+    '''
     def __init__(self):
+        '''
+        Constructor for the KeywordMatcher class.
+
+        :param self: Instance of the KeywordMatcher object
+        '''
         super().__init__()
 
         print("Keyword matcher loaded")
 
     def getSuggestions(self, question: str) -> List[str]:
+        '''
+        Given a question, determines which question subjects contain the same 
+        keywords and should be suggested.
+
+        :param self: Instance of the KeywordMatcher object
+        :param question: A question string
+        :return suggestions: List of suggested question subjects
+        '''
+        # create a dictionary mapping keywords to the subjects of previous questions
         pastQuestions = {"pointer": "How do I use pointers in C?", "code": "How do I code well?"}
         suggestions = []
 

c62824f14e8129e6a000552f1a49cb77e5b24070
fraser <frasergilbert184@gmail.com>
2021-08-14 13:57:39 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from app.interface import BasicCLI
 from app.domain import UniversalEncoder, questionmatcher
 from app.embedder import SentEmbeddings
-from app.formatter import DataParser
 from app.parser import parseQuestionsAnswersFromFile
+from app.storage import question_answers_to_json
+
 
 class App():
     def __init__(self) -> None:
-        questions, answers = parseQuestionsAnswersFromFile('app/testfiles/help2002-2017.txt')
+        questions, answers = parseQuestionsAnswersFromFile(
+            'app/testfiles/help2002-2017.txt')
+        question_answers_to_json(questions, answers)
         questionMatcher = UniversalEncoder(questions, answers)
         self.__cli = BasicCLI(questionMatcher)
 

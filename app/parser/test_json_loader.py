0537c39162858faf55f8743973cb6fe9bb9d313b
fraser <frasergilbert184@gmail.com>
2021-08-16 21:19:04 +0800

    Create test_json_loader.py

---
+++ app/parser/test_json_loader.py
@@ -0,0 +1,49 @@
+import unittest
+from json_loader import JsonLoader
+
+
+class TestJson(unittest.TestCase):
+
+    def test_json_storage(self):
+
+        file = 'testfile1.json'
+        json = JsonLoader(file)
+        actual1 = json.read_data()
+
+        for subject in actual1.keys():
+
+            self.assertEqual(actual1[subject]['Date'],
+                             'Wed Nov  1 13:07:21 2017')
+            self.assertEqual(actual1[subject]['To'],
+                             'help2002@csse.uwa.edu.au')
+            self.assertEqual(
+                actual1[subject]['Received'], 'from 101.177.104.187')
+            self.assertEqual(subject,
+                             'part 5 clarification')
+            self.assertEqual(actual1[subject]['From'],
+                             'poster033@student.uwa.edu.au')
+            self.assertEqual(actual1[subject]['X-smile'], None)
+            self.assertEqual(actual1[subject]['X-img'], 'none')
+            self.assertEqual(actual1[subject]['X-anonymous'], 'no')
+            self.assertEqual(actual1[subject]['Text_vec'], [0])
+            self.assertEqual(len(actual1[subject]['Answers']), 3)
+
+    def test_file_exists(self):
+        file = "TestFileExists.json"
+
+        json = JsonLoader(file)
+        actual1 = json.read_data()
+
+        self.assertEqual(actual1, None)
+
+    def test_file_json_readable(self):
+        file = 'testfile.jpg'
+
+        json = JsonLoader(file)
+        actual1 = json.read_data()
+
+        self.assertEqual(actual1, None)
+
+
+if __name__ == '__main__':
+    unittest.main()

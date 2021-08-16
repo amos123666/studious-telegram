164800eda4dcc9ce1eaa1264ce58396315b4e297
fraser <frasergilbert184@gmail.com>
2021-08-16 10:55:42 +0800

    Update basiccli.py
    Added print functions to make the while loop less crowded, subject for change

--- app/interface/basiccli.py
+++ app/interface/basiccli.py
@@ -5,12 +5,47 @@ from .userinterface import AbstractUserInterface
 class BasicCLI(AbstractUserInterface):
     __matcher: AbstractQuestionMatcher = None
 
-    def __init__(self, matcher: AbstractQuestionMatcher) -> None:
+    def __init__(self, matcher: AbstractQuestionMatcher, questions):
         super().__init__()
         self.setQuestionMatcher(matcher)
+        self.__questions = questions
 
     def setQuestionMatcher(self, matcher: AbstractQuestionMatcher):
         self.__matcher = matcher
+
+    def print_question(self, chosen_questions):
+        print()
+        print(f'Date: {self.__questions[chosen_questions]["Date"]}')
+        print(f'To: {self.__questions[chosen_questions]["To"]}')
+        print(
+            f'Received: {self.__questions[chosen_questions]["Received"]}')
+        print(f'Subject: { chosen_questions}')
+        print(f'From: {self.__questions[ chosen_questions]["From"]}')
+        print(f'X-smile: {self.__questions[ chosen_questions]["X-smile"]}')
+        print(f'X-img: {self.__questions[chosen_questions]["X-img"]}')
+        print()
+        print(self.__questions[chosen_questions]["Text"])
+        print()
+        print("---------------------------------------------")
+        print()
+
+    def print_answers(self, chosen_questions):
+        for answers in self.__questions[chosen_questions]['Answers']:
+            print(f'Date: {answers["Date"]}')
+            print(f'To: {answers["To"]}')
+            print(
+                f'Received: {answers["Received"]}')
+            print(f'Subject: {chosen_questions}')
+            print(f'From: {answers["From"]}')
+            print(
+                f'X-smile: {answers["X-smile"]}')
+            print(
+                f'X-img: {answers["X-img"]}')
+            print()
+            print(answers['Text'])
+            print()
+            print("---------------------------------------------")
+            print()
 
     def start(self):
         if self.__matcher == None:
@@ -24,3 +59,11 @@ class BasicCLI(AbstractUserInterface):
             print(f'QUESTIONS: {question}')
             for i in range(0, 10):
                 print(f"{i + 1}: {suggestions[i]}")
+
+            print()
+            selected = input("Please enter the suggested question number >> ")
+            print()
+            question_selected = suggestions[int(selected)-1]
+
+            self.print_question(question_selected)
+            self.print_answers(question_selected)

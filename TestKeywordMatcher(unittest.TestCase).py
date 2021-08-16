7dc3c00be9d0a64335fe691c0c0226f6f0329139
Amos <61882594+amos123666@users.noreply.github.com>
2021-08-16 18:57:12 +0800

    Update TestKeywordMatcher(unittest.TestCase).py

--- TestKeywordMatcher(unittest.TestCase).py
+++ TestKeywordMatcher(unittest.TestCase).py
@@ -5,9 +5,8 @@ from app.domain.keywordmatcher import KeywordMatcher
 class TestKeywordMatcher(unittest.TestCase):
     def test_keywordmatcher(self):
         # Please enter the keyword
-        keyword = input("Please enter the keyword: ")
-        suggestion = KeywordMatcher.getSuggestions(self,keyword)
-        print(suggestion)
+        self.assertEqual((KeywordMatcher.getSuggestions(self,"It is a code"))[0], "How do I code well?")
+        self.assertEqual((KeywordMatcher.getSuggestions(self, "we are a pointer"))[0], "How do I use pointers in C?")
 
 
 if __name__ == '__main__':

8ec71dcdf6327105a02ab2054f2debe29b9632a5
fraser <frasergilbert184@gmail.com>
2021-08-16 11:18:17 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,29 @@
 from app.interface import BasicCLI
-from app.domain import UniversalEncoder, questionmatcher
+from app.domain import UniversalEncoder, questionmatcher, SentBERT, Doc2Vec
 from app.embedder import SentEmbeddings
 from app.parser import parseQuestionsAnswersFromFile
 from app.parser import JsonLoader
 
 
 class App():
-    def __init__(self) -> None:
-        # parseQuestionsAnswersFromFile(
-        # 'app/testfiles/help2002-2017.txt') Uncomment this if you want to create a new json file
+    def __init__(self, targetModel="UniversalEncoder") -> None:
+
+        # Uncomment this if you want to create a new json file
+        '''
+        questions, answers = parseQuestionsAnswersFromFile(
+            'app/testfiles/help2002-2017.txt')
+        '''
         json = JsonLoader('app/storage/questions2017_UE.json')
         questions = json.read_data()
-        questionMatcher = UniversalEncoder(questions)
+        if targetModel == "UniversalEncoder":
+            questionMatcher = UniversalEncoder(questions)
+        elif targetModel == "BERT":
+            questionMatcher = SentBERT(questions)
+        elif targetModel == "doc2vec":
+            questionMatcher = Doc2Vec(questions)
+        else:
+            raise ValueError(f"targetModel ({targetModel}) is not valid")
+
         self.__cli = BasicCLI(questionMatcher, questions)
 
     def start(self) -> None:

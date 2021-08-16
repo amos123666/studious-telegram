eb918ca8a781451c911a35808d80af084c92d9e9
fraser <frasergilbert184@gmail.com>
2021-08-16 21:19:00 +0800

    Create test_parsing.py

---
+++ app/parser/test_parsing.py
@@ -0,0 +1,25 @@
+import unittest
+from parsing import get_vectors, parseThreadsFromFile
+
+
+class TestJParsing(unittest.TestCase):
+
+    def test_vectors(self):
+        string = "I need exam help."
+        actual = get_vectors(string)
+
+        self.assertEqual(isinstance(actual, list), True)
+        self.assertEqual(len(actual), 512)
+
+    def test_parsing_testfiles(self):
+        file = 'testposts.txt'
+        actual = parseThreadsFromFile(file)
+
+        expected1 = 'Date: Mon Jul 31 12:17:48 2017\nTo: help2002@csse.uwa.edu.au\nReceived: from 130.95.1.70\nSubject: Hello Everyone, welcome to CITS2002 Systems Programming in 2017\nFrom: chris.mcdonald@uwa.edu.au\nX-smilie: none\nX-img: none\n\nHello Everyone, welcome to CITS2002 Systems Programming in 2017.\n\nThanks, Chris.\n\n'
+        print(len(actual[0]))
+        print(len(expected1))
+        self.assertEqual(actual[0], expected1)
+
+
+if __name__ == '__main__':
+    unittest.main()

84dd362701ee43a4e7b1ad2b48b19581922de036
fraser <frasergilbert184@gmail.com>
2021-08-16 11:13:42 +0800

    Update __init__.py

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -2,3 +2,5 @@ from .questionmatcher import AbstractQuestionMatcher
 
 from .keywordmatcher import KeywordMatcher
 from .universalencoder import UniversalEncoder
+from .sent_BERT import SentBERT
+from .doc2vec import Doc2Vec

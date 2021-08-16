d8e2714f4ae0c88dfa5e6f24dd70616e9b4142ac
fraser <frasergilbert184@gmail.com>
2021-08-16 21:19:52 +0800

    Update json_loader.py

--- app/parser/json_loader.py
+++ app/parser/json_loader.py
@@ -1,5 +1,7 @@
+from genericpath import isfile
 import json
 from .loader import AbstractLoader
+import os
 
 
 class JsonLoader(AbstractLoader):
@@ -10,7 +12,14 @@ class JsonLoader(AbstractLoader):
     def read_data(self):
 
         # load in json data
-        with open(self.__file, 'r') as fp:
-            data = json.load(fp)
-
-        return data
+        if (os.path.isfile(self.__file)):
+            with open(self.__file, 'r') as fp:
+                try:
+                    data = json.load(fp)
+                except ValueError:
+                    print("Invalid File. Must be extension '.json'")
+                    return None
+            return data
+        else:
+            print("File does not exists or directory does not exist")
+            return None

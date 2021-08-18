e58d9957338ccb5b513692db2eb5e1a0de9c8645
fraser <frasergilbert184@gmail.com>
2021-08-18 09:40:52 +0800

    Update __init__.py

--- app/parser/__init__.py
+++ app/parser/__init__.py
@@ -1,3 +1,3 @@
-from .parsing import parseQuestionsAnswersFromFile
+from .parsing import parseQuestionsAnswersFromFile, get_vectors, parseThreadsFromFile
 from .loader import AbstractLoader
 from .json_loader import JsonLoader

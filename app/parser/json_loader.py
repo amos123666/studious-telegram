4e7c49ad03a72692e0ff18d31a1700a8bafe9f3a
Shana Edwards <82807298+shana-edwards@users.noreply.github.com>
2021-08-25 10:36:04 +0800

    documenting json_loader (#26)

--- app/parser/json_loader.py
+++ app/parser/json_loader.py
@@ -3,12 +3,27 @@ from .loader import AbstractLoader
 
 
 class JsonLoader(AbstractLoader):
+    '''
+    This is a class for loading data from a json file.
+    '''
 
     def __init__(self, file):
+        '''
+        Constructor for the JsonLoader class.
+
+        :param self: Instance of the JsonLoader object
+        :param file: json file from which we want to load data
+        '''
         self.__file = file
 
     def read_data(self):
+        '''
+        Access the json data from the given file and 'load' it into 
+        a dictionary.
 
+        :param self: Instance of the JsonLoader object
+        :return data: Dictionary containing json data
+        '''
         # load in json data
         with open(self.__file, 'r') as fp:
             data = json.load(fp)

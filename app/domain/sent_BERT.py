44aed3c6582f0f1a01e8114091a1ac8e4a867724
Shana Edwards <82807298+shana-edwards@users.noreply.github.com>
2021-08-25 10:35:38 +0800

    documenting sent_BERT (#25)

--- app/domain/sent_BERT.py
+++ app/domain/sent_BERT.py
@@ -7,19 +7,38 @@ import operator
 from typing import List
 
 class SentBERT(AbstractQuestionMatcher):
+    '''
+    This is a class for sentence embedding of question subjects using 
+    Sentence-BERT.
+    '''
     def __init__(self, questions):
+        '''
+        Constructor for the sentBERT class.
+
+        :param self: Instance of the sentBERT object
+        :param questions: Dictionary of question threads
+        '''
         # Load the model and pass in questions as a list to get embeddings
         self.__model = SentenceTransformer('bert-base-nli-mean-tokens')
         self.__question_list = list(questions.keys())
         self.__sentence_embeddings = self.__model.encode(self.__question_list)
 
     def getSuggestions(self, question: str) -> List[str]:
-        # pass the asked question into model to get embedding
+        '''
+        Determines question suggestions for a given question, based on the 
+        similarity of their subject-line.
+
+        :param self: Instance of the sentBERT object
+        :param question: An element of the question dictionary
+        :return [k[0] for k in similarity_dict]: List of all questions from 
+            question dictionary ordered from most similar to least
+        '''
+        # Pass the asked question into model to get embedding
         query_embedding = self.__model.encode([question])[0]
         query_embedding = query_embedding.reshape(-1, 1)
 
         # Loop through all the sentence embeddings and find cosine between
-        # the asked question embedding
+        # The asked question embedding
         similarity_dict = {}
         for i, sentence_embedding in enumerate(self.__sentence_embeddings):
             sentence_embedding = sentence_embedding.reshape(-1, 1)

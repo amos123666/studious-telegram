b4c8f73bb4855a81dffc3d5f6cbc499f93090f20
fraser <frasergilbert184@gmail.com>
2021-09-26 21:24:16 +0800

    Update __init__.py

--- qna/src/app/domain/__init__.py
+++ qna/src/app/domain/__init__.py
@@ -1,6 +1,7 @@
-from .questionmatcher import AbstractQuestionMatcher
-
-from .keywordmatcher import KeywordMatcher
-from .universalencoder import UniversalEncoder
-from .sent_BERT import SentBERT
 from .doc2vec import Doc2Vec
+from .sent_BERT import SentBERT
+from .universalencoder import UniversalEncoder
+from .keywordmatcher import KeywordMatcher
+from .questionmatcher import AbstractQuestionMatcher
+from .summarisation import AbstractSummarisation
+from.t5 import T5

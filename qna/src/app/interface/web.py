234e05b34ec7dd30dd2da92af02ce30d2ff6dac9
Alexander Shearer <alexshearer00@gmail.com>
2021-09-29 23:51:06 +0800

    Add posting questions back to webserver

--- qna/src/app/interface/web.py
+++ qna/src/app/interface/web.py
@@ -1,4 +1,7 @@
+import re
+from typing import Awaitable, Optional
 import urllib.parse
+import json
 
 import tornado.ioloop
 import tornado.web
@@ -7,10 +10,23 @@ from .userinterface import AbstractUserInterface
 from ..domain.questionmatcher import AbstractQuestionMatcher
 
 
-class QuestionHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
     def initialize(self, questionMatcher: AbstractQuestionMatcher) -> None:
         self.questionMatcher = questionMatcher
 
+    def prepare(self) -> Optional[Awaitable[None]]:
+        if "Content-Type" not in self.request.headers:
+            self.json_args = None
+            return
+
+        if self.request.headers["Content-Type"] != "application/json":
+            self.json_args = None
+            return
+
+        self.json_args = json.loads(self.request.body)
+
+
+class SuggestionHandler(BaseHandler):
     def get(self, rawQuestion) -> None:
         question = urllib.parse.unquote(rawQuestion)
 
@@ -30,6 +46,18 @@ class QuestionHandler(tornado.web.RequestHandler):
         self.write(response)
 
 
+class NewQuestionHandler(BaseHandler):
+    def post(self) -> None:
+        if self.json_args is None:
+            self.set_status(400)
+            return
+
+        self.questionMatcher.addQuestions(
+            [self.json_args['subject']], self.json_args['body'])
+
+        self.set_status(200)
+
+
 class TornadoWebInterface(AbstractUserInterface):
     def __init__(self, port, questionMatcher: AbstractQuestionMatcher) -> None:
         super().__init__()
@@ -39,7 +67,8 @@ class TornadoWebInterface(AbstractUserInterface):
 
     def start(self) -> None:
         app = tornado.web.Application([
-            (r"/api/suggestion/([^/]+)", QuestionHandler, {"questionMatcher": self.__questionMatcher})
+            (r"/api/suggestion/([^/]+)", SuggestionHandler, {"questionMatcher": self.__questionMatcher}),
+            (r"/api/question/new", NewQuestionHandler, {"questionMatcher": self.__questionMatcher}),
         ])
 
         app.listen(self.__port)

a7190ca4d46fb95ab51d0268fed3970d9cce20a1
Amos <61882594+amos123666@users.noreply.github.com>
2021-10-07 20:49:52 +0800

    Update loader.py

--- qna/src/app/parser/loader.py
+++ qna/src/app/parser/loader.py
@@ -2,7 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class AbstractLoader(ABC):
+    '''
+    Abstract data loader.
 
+    Concrete data loader implementations must be a subclass and implement
+    all abstract methods.
+    '''
     @abstractmethod
     def read_data(self):
         pass

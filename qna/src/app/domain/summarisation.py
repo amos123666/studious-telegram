70be4a4211ea33a9d684fb22334e59742acb973f
Amos <61882594+amos123666@users.noreply.github.com>
2021-10-07 21:01:51 +0800

    Update summarisation.py

--- qna/src/app/domain/summarisation.py
+++ qna/src/app/domain/summarisation.py
@@ -3,6 +3,12 @@ from typing import List
 
 
 class AbstractSummarisation(ABC):
+    '''
+    Abstract summarisation interface.
+
+    Concrete summarisation implementations must be a subclass and implement
+    all abstract methods.
+    '''
 
     @abstractmethod
     def getSummarisations(self, question: str) -> List[str]:
